a1adcd45471a4224fbdae31f69b51d850a8b5f46
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-19 15:44:02 -0500

    TYLER OWNED AR

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,30 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+
+  validates :email, :uniqueness => :true
+  validates :email, :format => { :with => /.+@.+\...+/, 
+      :message => "Not a valid email."}
+  validates :phone, :format => { :with => /\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*/, 
+      :message => "Phone numbers must include 10 digits."}
+      
+  # validates :birthday, :numericality => { :greater_than }
+  validates :age, :numericality => {:greater_than => 4}
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    ((Date.today - birthday)/365.25).floor.to_i
+  end
+
+  def check_email(email)
+    email =~ /.+@.+\...+/
+  end
+
+  def check_age
+    # errors.add_to_base('Must be at least 5 years old') if
+     age > 4
+  end
 end
---
+++ db/migrate/20121011144240_create_students.rb
@@ -0,0 +1,20 @@
+require_relative '../config'
+
+# this is where you should use an ActiveRecord migration to 
+
+class CreateStudents < ActiveRecord::Migration
+  def change
+    drop_table :students
+    
+    create_table :students do |t|
+      t.string    :first_name
+      t.string    :last_name
+      t.string    :gender
+      t.date  :birthday
+      t.string    :email
+      t.string    :phone
+      t.string    :address
+    end
+  end
+end
+
